18ae2f6640c7590f094c8ca505bfc9fbf31940dc
Andrew Clissold <andrewclissold@gmail.com>
2014-02-08 21:32:42 -0500

    Create skeleton classes

---
+++ src/com/andrewclissold/sort/Main.java
@@ -0,0 +1,13 @@
+package com.andrewclissold.sort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Sorter sorter = new Sorter();
+
+        sorter.sortAscending(new int[0]);
+        sorter.sortDescending(new int[0]);
+    }
+}
---
+++ src/com/andrewclissold/sort/Sorter.java
@@ -0,0 +1,20 @@
+package com.andrewclissold.sort;
+
+// Sorter provides simple ascending and descending sort facilities.
+public class Sorter {
+
+    // implicit default no-param constructor
+
+    // Sorts the input array in ascending order
+    public void sortAscending(int[] x) {
+        System.out.println("Sort ascending!");
+        // TODO Actually sort something
+    }
+
+    // Sorts the input array in descending order
+    public void sortDescending(int[] x) {
+        System.out.println("Sort descending!");
+        // TODO Actually sort something
+    }
+
+}
