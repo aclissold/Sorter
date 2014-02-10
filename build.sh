cc9cc7cf5694997d3ec4d27f426fd1a09aa06069
Andrew Clissold <andrewclissold@gmail.com>
2014-02-09 21:38:23 -0500

    Improve build script
    Now terminates on any error, rather than continuing on and causing
    problems further down the road.

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+set -e
 
 if [[ ! -d lib ]]
 then
     # Download dependencies
     echo "Downloading dependencies..."
-    mkdir -p lib
-    cd lib
     curl -O "http://search.maven.org/remotecontent?filepath=junit/junit/4.11/junit-4.11.jar"
     curl -O "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
-    cd ..
+    mkdir -p lib
+    mv ./*.jar lib
 fi
 
 # Ensure binary directory exists
