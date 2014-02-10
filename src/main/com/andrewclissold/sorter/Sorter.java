f6eee25c6a54d13d43d8bdf3f44fe285a07506c6
Andrew Clissold <andrewclissold@gmail.com>
2014-02-09 20:34:24 -0500

    Implement the sort methods (all tests pass!)
    Also tweaked the size of the test case arrays since the running time
    of bogosort is Ω(n · n!).

--- src/main/com/andrewclissold/sorter/Sorter.java
+++ src/main/com/andrewclissold/sorter/Sorter.java
@@ -1,18 +1,46 @@
 package com.andrewclissold.sorter;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+
 // Sorter provides simple ascending and descending sort facilities.
 public class Sorter {
 
     // implicit default no-param constructor
 
     // Sorts the input array in ascending order
-    public void sortAscending(int[] x) {
-        // TODO Actually sort something
+    public void sortAscending(int[] array) {
+        sort(false, array);
     }
 
     // Sorts the input array in descending order
-    public void sortDescending(int[] x) {
-        // TODO Actually sort something
+    public void sortDescending(int[] array) {
+        sort(true, array);
+    }
+
+    // Sort implements an even stupider version of bogosort. Bogosort, also
+    // known as random sort or stupidsort, randomly permutes the array until
+    // it is sorted. This implementation one-ups the stupidity of it by first
+    // calling Collections.sort on a copy of the array (hah!) to use as the
+    // comparison for whether or not the shuffled array is sorted.
+    private void sort(boolean descending, int[] array) {
+        // Setup
+        ArrayList<Integer> list = new ArrayList<Integer>(array.length);
+        for (int num : array) list.add(num);
+        ArrayList<Integer> sortedList = new ArrayList<Integer>(list);
+        Collections.sort(sortedList);
+        if (descending) Collections.reverse(sortedList);
+
+        // bogosort
+        while (!list.equals(sortedList)) {
+            Collections.shuffle(list);
+        }
+
+        // Put list back into the original array now that it's sorted
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
     }
 
 }
--- src/test/com/andrewclissold/sorter/SorterTest.java
+++ src/test/com/andrewclissold/sorter/SorterTest.java
@@ -77,12 +77,7 @@ public class SorterTest {
     @Test
     public void ascendingPermutes() {
         // Ensures the resultant array is a permutation of the original items.
-        int[] array = {2, -1, 2, -10, 4, 5, -5, -10, 5, -5, -4, 9, -9, 2,
-        -2, -3, 4, 3, -6, -9, -7, -8, 6, -5, 1, 5, -5, 5, -7, 10, -4, -2, -7,
-        -7, 5, 3, -4, -5, 5, -2, -5, -8, -7, -5, -1, 1, -8, -1, -4, 4, -3, -10,
-        -7, 8, 9, 10, -6, -6, -5, -5, -9, -7, 8, 1, 9, -2, 8, 10, -2, 6, -9,
-        -9, 6, 10, -5, -10, 3, -9, -6, 1, -8, 1, -3, -2, -5, 1, 6, -9, 7, -5,
-        6, 9, -3, -9, 7, -8, -10, 4, -2, 0};
+        int[] array = {2, 1, 2, 2, 2, 3, 3, 2, 3, 2};
 
         // Use a HashMaps to count the occurences of each item
         HashMap<Integer, Integer> expected = new HashMap<Integer, Integer>();
@@ -100,12 +95,7 @@ public class SorterTest {
     @Test
     public void descendingPermutes() {
         // Ensures the resultant array is a permutation of the original items.
-        int[] array = {2, -1, 2, -10, 4, 5, -5, -10, 5, -5, -4, 9, -9, 2,
-        -2, -3, 4, 3, -6, -9, -7, -8, 6, -5, 1, 5, -5, 5, -7, 10, -4, -2, -7,
-        -7, 5, 3, -4, -5, 5, -2, -5, -8, -7, -5, -1, 1, -8, -1, -4, 4, -3, -10,
-        -7, 8, 9, 10, -6, -6, -5, -5, -9, -7, 8, 1, 9, -2, 8, 10, -2, 6, -9,
-        -9, 6, 10, -5, -10, 3, -9, -6, 1, -8, 1, -3, -2, -5, 1, 6, -9, 7, -5,
-        6, 9, -3, -9, 7, -8, -10, 4, -2, 0};
+        int[] array = {2, 1, 2, 2, 2, 3, 3, 2, 3, 2};
 
         // Use a HashMaps to count the occurences of each item
         HashMap<Integer, Integer> expected = new HashMap<Integer, Integer>();
